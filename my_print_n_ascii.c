be2d08fb6b6457c437c041062ac9dd1aded2334f
Charl33s <167294983+Charl33s@users.noreply.github.com>
2024-04-17 12:29:18 +0000

    my_print_n_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -2,12 +2,6 @@ void print_char(char c);
 
 void my_print_n_ascii(int howMany);
 
-int main (void) 
-{
-    my_print_n_ascii(5);
-    return 0;
-}
-
 void my_print_n_ascii (int howmany){
     int a=howmany;
     if (a>127){
---
+++ my_print_revert.c
@@ -0,0 +1,10 @@
+void print_char(char c);
+
+void my_print_revert(char *toRevert);
+
+int main(void)
+{
+my_print_revert("codingclub");
+return 0;
+}
+
